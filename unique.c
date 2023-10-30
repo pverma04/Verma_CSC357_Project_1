3d81c5296259c213571c8b15adfee5687626912a
pverma04 <pverma02@calpoly.edu>
2023-10-30 13:38:20 -0700

    finshed proj1

--- unique.c
+++ unique.c
@@ -9,6 +9,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     FILE* f = fopen(argv[1], "r"); //open file to read
+
     if (f == NULL) {
         printf("Invalid file");
         return 1;
@@ -24,7 +25,16 @@ int main(int argc, char* argv[]) {
         if (prev_line == NULL || strcmp(prev_line, curr_line) != 0) { //either this is the first line or they are not the same - both cases print curr_line 
             printf("%s", curr_line);
         }
+        if(prev_line != NULL) {
+            free(prev_line);
+        }
         prev_line = strdup(curr_line); //set prev line to current for next iteration
+    }
+    if (prev_line != NULL) { //IN CASE prev_line wasn't freed before
+        free(prev_line);
+    }
+    if (curr_line != NULL) {
+        free(curr_line);
     }
     return 0;
 }
